6d93ea0369ec27c5b43e02b58e12b2e0f4cab9eb
Andrii Nemchenko <andriinemchenko@gmail.com>
2021-02-23 14:08:38 +0200

    save token

--- src/main/scala/bot.scala
+++ src/main/scala/bot.scala
@@ -1,7 +1,7 @@
 import zero.ext._, option._
-import zio._, nio._, core._, clock._, stream._, console._, system._
+import zio._, nio._, core._, clock._, stream._, console._, system._, IO.succeed
 import db._
-import ftier._, ws._, udp._, http._
+import ftier.{Err=>_,_}, ws._, udp._, http._
 import zd.proto.api._
 
 val app =
@@ -13,9 +13,24 @@ val app =
     store <- ZIO.environment[Store with ZEnv]
     _  <- httpServer.bind(
             addr,
-            IO.succeed(httpHandler.andThen(_.provide(store))),
-            IO.succeed(PartialFunction.empty),
+            succeed(httpHandler
+              .andThen(
+                _.tapError{
+                  case Attack => putStrLn("attack is detected")
+                  case NoSecret => putStrLn("bot secret is missing")
+                  case x: NotImplemented => putStrLn(x.toString)
+                  case x: SecurityException => putStrLn(x.toString)
+                }
+              ).andThen(
+                _.catchAll(_ => succeed(notfound))
+              ).andThen(
+                _.provide(store)
+              )
+            ),
+            succeed(PartialFunction.empty),
           )
   } yield ()).provideCustomLayer(Store.live("data"))
 
 @main def run(): Unit = Runtime.default.unsafeRun(app)
+
+given CanEqual[Err, Err] = CanEqual.derived
--- src/main/scala/rest.scala
+++ src/main/scala/rest.scala
@@ -3,34 +3,44 @@ import zero.ext._, option._
 import zio._, nio._, core._, clock._, stream._, console._, system._
 import db._
 import ftier.{telegram=>tg,_}, ws._, udp._, http._
-import zd.proto.api._
+import zd.proto._, api.MessageCodec, macrosapi._
 
-val httpHandler: PartialFunction[Request, ZIO[Store with ZEnv, Nothing, Response]] = {
+val httpHandler: PartialFunction[Request, ZIO[Store with ZEnv, Err, Response]] = {
   case Request("GET", Root / "acpo", _, _) => IO.succeed(response(acpoHtml, "text/html"))
 
   case Request("POST", (Root / "acpo" / "link") ? ("id"*id & "fiz_id"*fiz_id & "token"*token), _, _) =>
-    IO.succeed(notfound) //todo
+    for {
+      chatid <- ZIO.require(Attack)(get(Key(id.utf8)))
+      dat <- ((fiz_id, token)).encode.map(Dat.apply)
+      _ <- put(chatid.toKey, dat)
+    } yield response("Готово.".toChunk, "text/plain")
 
   case Request("POST", Root / "bot" / x, _, body) =>
-    (for {
-      secret <- ZIO.require(NoSecret)(env("botsecret"))
+    for {
+      secret <- ZIO.require[ZEnv, Err, String](NoSecret)(env("botsecret"))
       _ <- IO.when(x != secret)(IO.fail(Attack))
-      answer <- tg.reader.find(body) {
-                  case tg.Update.PrivateQuery(chatId, q) if q.isStart =>
-                    for {
-                      r <- IO.effectTotal(SecureRandom())
-                      xs <- IO.succeed(new Array[Byte](32))
-                      _ <- IO.effectTotal(r.nextBytes(xs))
-                      hex <- IO.effectTotal(xs.hex)
-                      _ <- put(Dat(hex), Dat(chatId.toBytes))
-                      url = s"https://bot2.nobodytells.me/acpo?id=${hex.utf8}"
-                      a  <- tg.writer.answerPrivateQuery(chatId, tg.QueryRes(url))
-                    } yield a
-                  case x => IO.fail(NotImplemented(x))
-                }
-    } yield response(answer, "application/json")).catchAll(e => for {
-      _ <- putStrLn(e.toString) //todo: move to bot.scala or remove
-    } yield notfound)
+      answer <-
+        tg.reader.find[Store with ZEnv, Err](body) {
+          case tg.Update.PrivateQuery(chatid, "/start") =>
+            for {
+              r <- IO.effectTotal(SecureRandom())
+              xs <- IO.succeed(new Array[Byte](32))
+              _ <- IO.effectTotal(r.nextBytes(xs))
+              id <- IO.effectTotal(xs.hex)
+              _ <- put(Key(id), Dat(chatid.toBytes))
+              url = s"https://bot2.nobodytells.me/acpo?id=${id.utf8}"
+              a  <- tg.writer.answerPrivateQuery(chatid, tg.QueryRes(url))
+            } yield a
+          case tg.Update.PrivateQuery(chatid, "acpo") =>
+            for {
+              dat <- ZIO.require(Attack)(get(Key(chatid.toBytes)))
+              ft <- dat.bytes.decode[(String,String)]
+              (fiz_id, token) = ft
+              a  <- tg.writer.answerPrivateQuery(chatid, tg.QueryRes("спробуйте пізніше"))
+            } yield a
+          case x => IO.fail(NotImplemented(x))
+        }
+    } yield response(answer, "application/json")
 }
 
 val notfound =
@@ -48,4 +58,10 @@ def headers(len: Long, ct: String): Map[String, String] =
 
 object NoSecret
 object Attack
-class NotImplemented(x: tg.Update)
+case class NotImplemented(x: tg.Update)
+
+type Err = NoSecret.type | Attack.type | NotImplemented | SecurityException
+
+given MessageCodec[Tuple2[String,String]] = caseCodecIdx
+
+given CanEqual[String, tg.Query] = CanEqual.derived
