d1652ea1526c6683afed5a1d34e7cc0858452e8d
Andrii Nemchenko <andriinemchenko@gmail.com>
2021-02-23 18:41:23 +0200

    buttons

--- src/main/scala/rest.scala
+++ src/main/scala/rest.scala
@@ -3,7 +3,7 @@ import zero.ext._, option._
 import zio._, nio._, core._, clock._, stream._, console._, system._
 import zio.IO.{succeed, effectTotal, when, fail}
 import db._
-import ftier.{telegram=>tg,_}, ws._, udp._, http._
+import ftier._, tg._, ws._, udp._, http._
 import zd.proto._, api.MessageCodec, macrosapi._
 
 val httpHandler: PartialFunction[Request, ZIO[Store with ZEnv, Err, Response]] = {
@@ -21,8 +21,16 @@ val httpHandler: PartialFunction[Request, ZIO[Store with ZEnv, Err, Response]] =
       secret <- ZIO.require[ZEnv, Err, String](NoSecret)(env("botsecret").catchAll(_ => IO.none)) //todo: pass as param
       _ <- when(x != secret)(fail(Attack))
       answer <-
-        tg.reader.find[Store with ZEnv, Err](body) {
-          case tg.Update.PrivateQuery(chatid, "/start") =>
+        reader.find[Store with ZEnv, Err](body) {
+          case Update.PrivateQuery(chatid, "/start") =>
+            writer.answerPrivateQuery(chatid, QueryRes("Вітаю"),
+              ReplyKeyboardMarkup(
+                ("acpo/login" :: "acpo/get" :: Nil) ::
+                Nil
+              ).some
+            )
+
+          case Update.PrivateQuery(chatid, "acpo/login") =>
             for {
               r <- effectTotal(SecureRandom())
               xs <- succeed(new Array[Byte](32))
@@ -30,10 +38,10 @@ val httpHandler: PartialFunction[Request, ZIO[Store with ZEnv, Err, Response]] =
               id <- effectTotal(xs.hex)
               _ <- put(Key(id), Dat(chatid.toBytes))
               url = s"https://bot2.nobodytells.me/acpo?id=${id.utf8}"
-              a  <- tg.writer.answerPrivateQuery(chatid, tg.QueryRes(url))
+              a  <- writer.answerPrivateQuery(chatid, QueryRes(url))
             } yield a
 
-          case tg.Update.PrivateQuery(chatid, "acpo") =>
+          case Update.PrivateQuery(chatid, "acpo/get") =>
             for {
               dat <- get(Key(chatid.toBytes))
               res <-
@@ -44,8 +52,8 @@ val httpHandler: PartialFunction[Request, ZIO[Store with ZEnv, Err, Response]] =
                       (fiz_id, token) = ft
                     } yield "наразі функціонал не працює"
                   case None =>
-                    succeed("виконайте /start знову")
-              a <- tg.writer.answerPrivateQuery(chatid, tg.QueryRes(res))
+                    succeed("виконайте acpo/login знову")
+              a <- writer.answerPrivateQuery(chatid, QueryRes(res))
             } yield a
 
           case _ => succeed("{}".toChunk)
